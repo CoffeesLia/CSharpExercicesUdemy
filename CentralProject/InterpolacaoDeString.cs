38a23d11d506fba06c231f2f2ad6a417ace94ed9
Cecília Melgaço <98719299+CoffeesLia@users.noreply.github.com>
2024-11-22 14:27:49 -0300

    Update InterpolacaoDeString.cs

--- CentralProject/InterpolacaoDeString.cs
+++ CentralProject/InterpolacaoDeString.cs
@@ -12,7 +12,9 @@ namespace CursoCSharp.Fundamentos
                 string marca = "Dell";
                 double preco = 5800.00;
 
-                Console.WriteLine("O " + nome + " da marca " + marca + " custa R$" + preco + ".");
+                Console.WriteLine("O " + nome + " da marca " + marca + " custa R$" + preco + "."); // formatação por concatenação que gruda um tipo ao outro
+                Console.WriteLine("O {0} da marca {1} custa R$ {1}.", nome, marca, preco); // formatação em que número substitui os números
+                Console.WriteLine($"O {nome} da marca {marca} custa R$ {preco}."); // nome da variável é colocado entre chaves 
             }
         }
     }
